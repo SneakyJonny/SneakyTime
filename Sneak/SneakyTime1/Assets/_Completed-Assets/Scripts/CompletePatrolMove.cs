4a8822e82876dc19c807b0451d0ff47f556eba1c
laupski <pkawalkowski@gmail.com>
2017-10-19 08:07:57 -0400

    Paul's Part
    0.1

---
+++ Sneak/SneakyTime1/Assets/_Completed-Assets/Scripts/CompletePatrolMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompletePatrolMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position += transform.up * Time.deltaTime * 3;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+        if (other.gameObject.CompareTag("Wall") || other.gameObject.CompareTag("Background"))
+        {
+            transform.Rotate(new Vector3(0, 0, 180));
+        }
+    }
+}
